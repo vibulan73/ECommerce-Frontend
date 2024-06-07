23f33525ce004a3d2284e068014a7fc135bd2b07
Vibulan Jeyarasa <120412626+vibulanj@users.noreply.github.com>
2024-06-07 11:43:33 +0530

    Nav Bar

---
+++ index.js
@@ -0,0 +1,17 @@
+var sidenav = document.querySelector('.side-navbar');
+
+function showNavBar() 
+{
+    sidenav.style.display ="block";
+    // sidenav.style.left =0;
+}
+function closeNavBar() {
+    sidenav.style.display ="none";
+    // sidenav.style.left ="-60%";
+}
+
+function showNavBar() 
+{
+    sidenav.style.display ="block";
+    // sidenav.style.left =0;
+}
