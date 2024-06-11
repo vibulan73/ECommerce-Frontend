112fb063d6e2bd4bf14c4dd06136d9a8486576d0
Vibulan Jeyarasa <120412626+vibulanj@users.noreply.github.com>
2024-06-11 11:12:24 +0530

    Content & Footer

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var sidenav = document.querySelector('.side-navbar');
 
-function showNavbar() 
-{
+function showNavbar() {
     // sidenav.style.display ="block";
-    sidenav.style.left ="0";
+    sidenav.style.left = "0";
 }
 
 function closeNavbar() {
     // sidenav.style.display ="none";
-    sidenav.style.left ="-60%";
+    sidenav.style.left = "-60%";
 }
 
